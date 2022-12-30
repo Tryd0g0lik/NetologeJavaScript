08329fbc3712edcc80f28a38c3f8ca70ba642a94
Tryd0g0lik <work80@mail.ru>
2022-12-30 14:45:02 +0700

    The code rewrited from '*.ts' files. The '*.js' files will be created

---
+++ 1.3_ООП_и_функции/functions.ts
@@ -0,0 +1,9 @@
+export function _generatingId(_lis:any[]){
+    let _lengs: number = _lis.length
+
+    if (_lengs == 0 || _lengs == null){
+        return 0
+    }
+
+    return _lengs
+}
---
+++ 1.3_ООП_и_функции/goodJS.ts
@@ -0,0 +1,38 @@
+// interface MainGood {
+//     id: number;
+//     name: string;
+//     description: string;
+//     size: number;
+//     price: number;
+//     availble: boolean;
+// }
+
+
+
+class Good {
+    id: number;
+    name: string;
+    description: string;
+    size: number;
+    price: number;
+    availble: boolean;
+    constructor (
+        id = 0,
+        names, descriptions,
+        sizes = 0,
+        prices = 0,
+        availbles = false,
+
+    ){
+        this.id = id;
+        this.name = names;
+        this.description = descriptions;
+        this.size = sizes;
+        this.price = prices;
+        this.availble = availbles;
+    }
+
+
+}
+
+export{ Good,  }
