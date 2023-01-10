676c44289107849fd44af6f19e3dc678c4d4c81b
Tryd0g0lik <work80@mail.ru>
2023-01-10 17:12:37 +0700

    1.2 the task 1 complated

--- 1.2. Операторы цикла, функции и объекты/functions.js
+++ 1.2. Операторы цикла, функции и объекты/functions.js
@@ -1,29 +1,66 @@
-function cycles(){
+/*
 
-	let number = prompt ('Insert a number: ')
-	let user_response = Number(number) //require('Please insert the integer more then zero: ')
+Задание 1 - сложный абзац для понимания - что конкретно требуется. Распилить бы его
+>> В отдельном файле с расширением .js напишите программу, состоящую из функции и кода, вызывающего ее с аргументом,
+>> значение которого получается из аргумента командной строки при запуске (используя process.argv[2] для получения
+>> первого аргумента
+Понял одно, что при вызове функции должен присутствовать аргумент в котором числа.
 
-	
-	if (
-		user_response > 0 == true
-		){
-			let _lists = [];
-			let integer_lists = [];
-			
-			for (let i = 1; i <= user_response; i++ ){
-			
-				if (user_response % i === 0){
-					_lists.push(user_response / i)
-				 }
+
+Далее...
+>> Вызываемая функция должна принимать параметр - количество
+>> первых простых чисел, которые необходимо найти.
+
+То ли должен быть списо среди которого есть простые числа,
+то ли должен быть список простых чисел,
+то ли должно быть 2 аргумента. Один  - список чисел, Второй указывает какое количество необходимо найти тех самых
+ простых чисел.
+
+ Как то все двухсмыслено описано , а уточнять негде. В месенджаре полная глушь, ответов не дождаться.
+ */
+
+function cycles(arr=[], integ=0){ // v.2
+	/*
+	Исходя из нашего общения
+	>> Она не принимает количество простых чисел в качестве аргумента и
+	>> не возвращает массив простых чисел.
+
+	Задание понима. следующее /=> На вход массив чисел
+
+	Из данного комментария
+	>> ... который ее вызывает не получает требуемое количество простых чисел
+
+	что все же должен быть второй аргумент задающий требуемое кол-во простых чисел.
+	 */
+	let __tr = true;
+	let __i = 0;
+	let __lists=[];
+	try {
+		for (__i; __i < arr.length; __i++) {
+			if (__lists.length === integ
+			&& integ !== 0 ) {
+				console.log('И все таки оно делится!')
+				console.log(__lists)
+				return __lists
 			}
-			
-			if (_lists.length === 2){
 
-				return alert("Ta-daa: " + user_response + " It's integer")
+			if (Number(arr[__i]) % 2 === 0) {
+				__lists.push(arr[__i]);
+
 			}
-			
-			return alert("Your nomber: " + user_response + " It's not integer")
 		}
-		
-	alert('ERROR!')
+		if (__lists.length > 0){
+			console.log('И все таки оно делится!')
+			console.log(__lists)
+		}
+		return __lists
+	} catch (e){
+		console.log(`ERROR of the "cycles_v2" Message: ${e.message}`)
+		console.log(`ERROR of the "cycles_v2" Stack: ${e.stack}`)
+	}
+
+
 }
+let integ = 3;
+let arr = [ 2, 3, 5, 7, 11, 13, 14, 17, 24, 28, 30]
+cycles(arr)
