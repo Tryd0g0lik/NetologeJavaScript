8382040b9afedf5ab7bfa4d61c3eddbd83d58605
Tryd0g0lik <work80@mail.ru>
2023-01-15 16:35:04 +0700

    README been added

--- 1.4. Обработка ошибок, замыкания и асинхронность/password.js
+++ 1.4. Обработка ошибок, замыкания и асинхронность/password.js
@@ -11,26 +11,80 @@ const result = require('dotenv').config({path: "./.env"}) //.env
 // const MongoClient = require('mongodb').MongoClient;
 
 const psw = result.parsed['PASS'];
-let __answered = async ()=> {
+
+// let __answered2 = ()=> {
+//     const rl = readline.createInterface({
+//         input: process.stdin, // The input's data
+//         output: process.stdout
+//     });
+//
+//
+//
+//
+//     return async function() {
+//         let userInp = await rl.question('Введите пароль\n');
+//         return userInp
+//     };
+// }
+
+// console.clear()
+// let data = __answered2()
+// console.log(data().then((dt)=> console.log(dt)))
+// data.then((dt)=> console.log(dt))
+
+// ------------------------------
+
+
+async function userInput() {
 
     const rl = readline.createInterface({
         input: process.stdin, // The input's data
         output: process.stdout
     });
 
-    const __resp = ()=> {
-        rl.question('Введите пароль\n')
 
-        return (userInpute) => {
-            console.log(userInpute)
+    let res = await rl.question('Введите пароль\n');
+    console.log(res + " 111111111111")
+    rl.close();
+    return
+};
 
-        }
+
+((data)=> {
+
+    console.log(data + "22222222");
+    if (data === psw) {
+        console.log(data);
+        return "true";
+    } else {
+        return "false";
     }
-     rl.close();
-    return await __resp();
-    }
-let data = __answered()
+}) (userInput())
+userInput()
     .then((data)=> {
-        console.log(data)
 
-    });
+        console.log(data + "22222222");
+        if (data === psw) {
+            console.log(data);
+            return "true";
+        } else {
+            return "false";
+        }
+})
+
+
+// userInput()
+//     .then((err, data)=> {
+//         console.log(data + "22222222");
+//         if (data === psw) {
+//             console.log(data);
+//             return "true";
+//         } else {
+//             return "false";
+//         }
+//     })
+
+
+
+
+// ------------------------------
