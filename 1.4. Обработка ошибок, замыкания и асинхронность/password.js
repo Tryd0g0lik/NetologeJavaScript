7df662ed5c8f984d18d0a34a7372c91ab9448edc
Tryd0g0lik <work80@mail.ru>
2023-01-15 10:12:34 +0700

    return [Function (anonymous)]

--- 1.4. Обработка ошибок, замыкания и асинхронность/password.js
+++ 1.4. Обработка ошибок, замыкания и асинхронность/password.js
@@ -1,94 +1,36 @@
 // const rlp = require('node:readline/promises');
 
-const rl = require('node:readline');
-const fs = require("fs");
+const readline = require('readline');
+
 const vsprintf = require('sprintf-js').vsprintf;
-const {describe, it} = require('test');
-const test = require('test');
-const assert = require("assert");
+// const {describe, it} = require('test');
+// const test = require('test');
+// const assert = require("assert");
 
+const result = require('dotenv').config({path: "./.env"}) //.env
+// const MongoClient = require('mongodb').MongoClient;
 
-let data;
-const passies = async (p)=>{
-    return await fs.promises.readFile('./user.json', 'utf-8')
-}
+const psw = result.parsed['PASS'];
+let __answered = async ()=> {
 
-let p = "12333"
-// pass(p)
-// function testing(p) {
-//     return passies(p)
+    const rl = readline.createInterface({
+        input: process.stdin, // The input's data
+        output: process.stdout
+    });
 
+    const __resp = ()=> {
+        rl.question('Введите пароль\n')
 
-// passies(p)
-//     .then((data) => {
-//         let __result = JSON.parse(data)
-//         let __boolenResult = (p === vsprintf("%s", [__result['PASS'],]));
-//         return __boolenResult
-//     });
+        return (userInpute) => {
+            console.log(userInpute)
 
+        }
+    }
+     rl.close();
+    return await __resp();
+    }
+let data = __answered()
+    .then((data)=> {
+        console.log(data)
 
-
-
-// describe("This's start!", (p)=> {
-//     it("is's first = true",  (p) => {
-//         assert.equal(
-//             (passies(p)
-//                 .then((data) => {
-//                     let __result = JSON.parse(data)
-//                     let __boolenResult = (p === vsprintf("%s", [__result['PASS'],]));
-//                     // console.log(__boolenResult)
-//                     return __boolenResult
-//                 }) == false), false
-//
-//         )
-//     })
-//     it("is's socond = false",  (p="456") => {
-//
-//         assert.equal(
-//             (passies(p)
-//                 .then((data) => {
-//                     let __result = JSON.parse(data)
-//                     let __boolenResult = (p === vsprintf("%s", [__result['PASS'],]));
-//                     // console.log(__boolenResult)
-//                     return __boolenResult
-//                 }) == false), false
-//
-//         )
-//     })
-// });
-
-
-// function testing(p){
-//     return passies(p)
-//         .then((data) => {
-//             let __result = JSON.parse(data)
-//             let __boolenResult = (p === vsprintf("%s", [__result['PASS'],]));
-//             return __boolenResult
-//         });
-// }
-// test('the data is peanut butter', async () => {
-//   const data = await testing(p);
-//   expect(data).toBe(false);
-// });
-//
-// test('the fetch fails with an error', async () => {
-//   expect.assertions(1);
-//   try {
-//     testing(p);
-//   } catch (e) {
-//     expect(e).toMatch('error');
-//   }
-// });
-
-// console.log(process.env)
-// const envy = require('envy');
-// const env = envy();
-// console.log(env);
-
-require('dotenv').config();
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect(process.env.PASS, function(err, db) {
-  if(!err) {
-    console.log("We are connected");
-  }
-});
+    });
