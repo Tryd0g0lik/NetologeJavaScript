5b5e7f5f88791a7897174c80961a9240659b8d14
Tryd0g0lik <work80@mail.ru>
2023-01-18 08:32:37 +0700

    rewrited the 'bascet' function

--- 1.2. Операторы цикла, функции и объекты/shop.js
+++ 1.2. Операторы цикла, функции и объекты/shop.js
@@ -12,8 +12,8 @@ let f = {"products":[
     {"id":7,"name":"Сироп","descriptions":"LA-LA-LA-LA","sizes":1050,"price":1355,"avaible":"true"},
     {"id":8,"name":"Пирожок","descriptions":"LA-LA-LA-LA","sizes":1050,"price":1355,"avaible":"true"},
     {"id":9,"name":"Пирожок","descriptions":"LA-LA-LA-LA","sizes":1050,"price":1355,"avaible":"true"},
-    {"id":10,"name":"Пирожок","descriptions":"LA-LA-LA-LA","sizes":1050,"price":1355,"avaible":"false"}
-    {"id":11,"name":"Клюква","descriptions":"LA-LA-LA-LA","sizes":1050,"price":1355,"avaible":"false"}
+    {"id":10,"name":"Пирожок","descriptions":"LA-LA-LA-LA","sizes":1050,"price":1355,"avaible":"false"},
+    {"id":11,"name":"Клюква","descriptions":"LA-LA-LA-LA","sizes":1050,"price":1355,"avaible":"false"},
     ]
 }
 
@@ -66,6 +66,7 @@ let size;
 let price;
 let availble;
 let totalpriceALL;
+let basketCatalog = {"bascetCount": []};
 function addProducts(newData = null) {
     /*
     TODO: The position will be added into catalog after checks. It will checked the 'id' number id presence or be not
@@ -89,22 +90,25 @@ function addProducts(newData = null) {
 function basketGood(
     id=0,
     name, description,
-    siae, prices,
+    size, price,
     availble
     ) {
     /*
     Тhe  shop basket
      */
+    let len;
 
     if (id === 0) {
-        let len = (f["products"]).length;
+        len = (f["products"]).length;
+    } else {
+        len = id;
     }
 
     //Создайте в коде несколько (не менее 5) товаров в каталоге и несколько (не менее 2) товаров в корзине.
 
 
     // Добавление товара в каталог
-    newData = {
+    let newData = {
         "id": len,
         "name": name,
         "descriptions": description,
@@ -119,90 +123,77 @@ function basketGood(
 // Реализуйте функции добавления товара в корзину
 function bascet(i, count) {
     /*
-    TODO: The amount items insert
-    Params: 'i' - index;
+    TODO: The amount items insert. Need the find product index from 'f' catalog and to append the 'cont' properties,
+      it's integer.
+    Params: 'i' - product index of catalog;
             'count' - The total count .
+            'basketCatalog' -   Thi is catalog from the basket.
      */
-    this.amount = count;
-    try {
-        let f = JSON.parse(fs.readFileSync("./root.json"));
-        for (let i = 0; i < (f["products"]).length; i++) {
-            if (f["products"][i].id === i) {
-                let prod = f["products"][i];
-                let count = JSON.parse(fs.readFileSync("./bascetAmount.json"))
-                prod["amount"] = this.amount;
-                if (checkId(count["bascetCount"], i) === true) {
-                    (count["bascetCount"]).push(prod);
-                    fs.writeFile("./bascetAmount.json", JSON.stringify(count), 'utf-8', (err) => {
-                        if (err) {
-                            console.log(`ERRORE 'bascet' Message: ${err.message}`);
-                            console.log(`ERRORE 'bascet' Stack: ${err.stack}`);
-                        }
-                        else {
-                            console.log("Rewrite the file! Ok");
-                        }
-                    });
-                }
-                else {
-                    console.log(`Backet has this's position id: ${i}`);
-                }
+    let amount = count;
+
+    for (let i = 0; i < (f["products"]).length; i++) {
+        if (f["products"][i].id === i) {
+            let prod = f["products"][i];
+
+            prod["amount"] = amount;
+            if (checkId(basketCatalog["bascetCount"], i) === true) {
+                (basketCatalog["bascetCount"]).push(prod);
+
+            }
+            else {
+                console.log(`Backet has this's position id: ${i}`);
+
             }
         }
     }
-    catch (e) {
-        console.log(`ERRORE basketGood Message: ${e.message}`);
-        console.log(`ERRORE basketGood Stack: ${e.stack}`);
-    }
+    return basketCatalog
 }
 
-    // Реализуйте функцию вычисления общего количества и стоимости товаров в корзине
-    totalAmount(path = null) { // ./bascetAmount.json
-        /*
-        The total price calculate;
-         */
+// Реализуйте функцию вычисления общего количества и стоимости товаров в корзине
+function totalAmount(path = null) { // ./bascetAmount
+    /*
+    The total price calculate;
+     */
 
-        let i;
-        try {
-            if (path !== null) {
-                let fBaskets = JSON.parse(fs.readFileSync(path));
-                fs.close
+    let i;
 
-                // fBaskets["bascetCount"] = this.removeUnavailable(fBaskets["bascetCount"]);
+    if (path !== null) {
+        let fBaskets = JSON.parse(fs.readFileSync(path));
+        fs.close
 
-
-                for (i = 0; i < (fBaskets["bascetCount"]).length; i++) {
-                    this.totalprice = (fBaskets["bascetCount"][i].prices *
-                        fBaskets["bascetCount"][i].amount);
-
-                    fBaskets["bascetCount"][i]["totalprice"] = this.totalprice;
-                }
+        // fBaskets["bascetCount"] = this.removeUnavailable(fBaskets["bascetCount"]);
 
 
-                fs.writeFile("./totalAmountBasket.json", JSON.stringify({ "totalPrices": fBaskets["bascetCount"] }), "utf-8", (err) => {
-                    if (err) {
-                        console.log(`WriteFile ERRORE in  Basket a "totalAmount". Message: ${err.message} `);
-                        console.log(`WriteFile ERRORE in  Basket a "totalAmount". Stack: ${err.stack}`);
-                    }
-                    else {
-                        console.log("Rewrite  the 'totalAmountBasket' file! Ok");
-                    }
-                });
+        for (i = 0; i < (fBaskets["bascetCount"]).length; i++) {
+            this.totalprice = (fBaskets["bascetCount"][i].prices *
+                fBaskets["bascetCount"][i].amount);
+
+            fBaskets["bascetCount"][i]["totalprice"] = this.totalprice;
+        }
 
 
-                for (i = 0; i < (fBaskets["bascetCount"]).length; i++) {
-
-                    this.totalpriceALL = Number(this.totalpriceALL) + Number(fBaskets["bascetCount"][i].totalprice);
-
-                }
-                return ` ${this.totalpriceALL} рубля;`;
+        fs.writeFile("./totalAmountBasket.json", JSON.stringify({ "totalPrices": fBaskets["bascetCount"] }), "utf-8", (err) => {
+            if (err) {
+                console.log(`WriteFile ERRORE in  Basket a "totalAmount". Message: ${err.message} `);
+                console.log(`WriteFile ERRORE in  Basket a "totalAmount". Stack: ${err.stack}`);
             }
-            return;
+            else {
+                console.log("Rewrite  the 'totalAmountBasket' file! Ok");
+            }
+        });
+
+
+        for (i = 0; i < (fBaskets["bascetCount"]).length; i++) {
+
+            this.totalpriceALL = Number(this.totalpriceALL) + Number(fBaskets["bascetCount"][i].totalprice);
+
         }
-        catch (e) {
-            console.log(`ERRORE in  Basket a "totalAmount". Message: ${e.message}`);
-            console.log(`ERRORE in  Basket a "totalAmount". Stack: ${e.stack}`);
-        }
+        return ` ${this.totalpriceALL} рубля;`;
     }
+    return;
+
+
+}
 
     totalSum(path) { // ./totalAmountBasket.json
         /*
