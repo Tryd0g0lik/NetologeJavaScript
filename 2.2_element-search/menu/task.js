49631e92a6e09434601d8e5e36761d62db09d7ba
Tryd0g0lik <work80@mail.ru>
2023-01-29 08:39:19 +0700

    added the folder 2.2_element-search

---
+++ 2.2_element-search/menu/task.js
@@ -0,0 +1,22 @@
+console.log("dddddddd");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");		
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+console.log("hello word");
+
+0
