aa0566533366f0a4d2292acd40af8c875152051b
Tryd0g0lik <work80@mail.ru>
2023-02-01 14:05:49 +0700

    task2_1: code change of the 'mole-game'

--- 2.1_JS_in_browser/mole-game/task.js
+++ 2.1_JS_in_browser/mole-game/task.js
@@ -1,18 +1,27 @@
-let i =0;
-    reputation = 0;
-    minusing = 0;
-window.addEventListener('click', ()=>{
-    let result = document.getSelection('.hole');
-    if (Number(i) < 14) {
-        if (((result.anchorNode).className).includes('hole_has-mole') === true) {
-            reputation++;
-        } else if (((result.anchorNode).className).includes('hole_has-mole') !== true) minusing++;
+let i = 0;
+reputation = 0;
+minusing = 0;
+window.addEventListener('click', () => {
+	let result = document.getSelection('.hole');
+	let dead = document.getElementById('dead');
+	let lost = document.getElementById('lost');
 
-        if (Number(minusing) === 5) document.location.assign('./img.png');
-        else if (Number(reputation) === 10) {
-            location.assign('./img_1.png')
-        }
-    }
+	if (Number(i) < 14) {
+		if (((result.anchorNode).className).includes('hole_has-mole') === true) {
+			reputation++;
+			dead.innerText = String(reputation);
+
+		} else if (((result.anchorNode).className).includes('hole_has-mole') !== true) {
+			minusing++;
+			lost.innerText = String(minusing);
+
+		}
+
+		if (Number(minusing) === 5) document.location.assign('./img.png');
+		else if (Number(reputation) === 10) {
+			location.assign('./img_1.png')
+		}
+	}
 })
 
 
