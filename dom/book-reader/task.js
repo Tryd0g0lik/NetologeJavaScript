43106c585e8fc29699ed654d3362a5f79dcf8ee2
Tryd0g0lik <work80@mail.ru>
2023-02-09 18:20:27 +0700

    averything is conpleted

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -11,12 +11,13 @@ class ContantSize {
         }
         return;
     }
+    /* font size - start */
     controlFontSize() {
         return this.htmlBlock.getElementsByClassName('font-size');
     }
-    findActiveSizeBook() {
+    removeActiveSizeBook() {
         /*
-        TODO: The 'font-size_active' class finding only.
+        TODO: The 'font-size_active' class finding and remove active classe.
         */
         this.fontSizes = this.controlFontSize();
         let i = 0;
@@ -28,7 +29,7 @@ class ContantSize {
         }
         return;
     }
-    findSizeClick() {
+    findFSizeClick() {
         /*
         TODO: the element finding wich has an event. It's for assingment the 'active' class
         */
@@ -37,15 +38,20 @@ class ContantSize {
         for (i; i < this.fontSizes.length; i++) {
             let elem = this.fontSizes[i];
             this.fontSizes[i].addEventListener('click', () => {
-                this.findActiveSizeBook();
+                this.removeActiveSizeBook();
                 if (elem.classList.contains('font-size_active') === false) {
                     elem.classList.add('font-size_active');
-                    this.insertActivFontSize();
+                    this.activFontSize();
                 }
             });
         }
     }
-    insertActivFontSize() {
+    activFontSize() {
+        /*
+        
+        TODO: the new element wich has get the new  class 'active'
+        
+        */
         let size;
         let activBlock = (this.htmlBlock.getElementsByClassName('font-size_active'));
         if (activBlock[0].getAttribute('data-size') &&
@@ -57,18 +63,149 @@ class ContantSize {
             size = 'book_fs-big';
         }
         ;
-        if (this.htmlBlock.classList.contains(size) === false)
-            this.htmlBlock.className = 'book';
-        if (size !== null)
-            this.htmlBlock.classList.add(size);
+        for (let elem of ['book_fs-small', 'book_fs-big']) {
+            if (elem !== size && this.htmlBlock.classList.contains(elem))
+                this.htmlBlock.classList.remove(elem);
+            if (size !== null)
+                this.htmlBlock.classList.add(size);
+        }
         return;
     }
-    fontColor() {
+    /* font size - end */
+    /* font color - start */
+    controlFColor() {
+        return this.htmlBlock.getElementsByClassName('book__control_color');
     }
+    removeActiveFColor() {
+        /*
+        TODO: The 'color_active' class finding only from a book__control_color and remove active classe.
+        */
+        let colors = this.controlFColor();
+        let activeColor = colors[0].getElementsByClassName('color');
+        let i = 0;
+        for (i; i < activeColor.length; i++) {
+            if (activeColor[i].classList.contains('color_active')) {
+                activeColor[i].classList.remove('color_active');
+                return i;
+            }
+        }
+        return;
+    }
+    findColorClick() {
+        /*
+        TODO: the element finding wich has an event. It's for assingment the 'active' class
+        */
+        let colorsBlock = this.controlFColor();
+        let refColor = colorsBlock[0].getElementsByClassName('color');
+        let ind = 0;
+        for (ind; ind < refColor.length; ind++) {
+            let elem = refColor[ind];
+            refColor[ind].addEventListener('click', () => {
+                this.removeActiveFColor();
+                if (elem.classList.contains('color_active') === false) {
+                    elem.classList.add('color_active');
+                    this.activFontColor();
+                }
+            });
+        }
+    }
+    activFontColor() {
+        /*
+        
+        TODO: the new element wich has get the new  class 'active'
+        
+        */
+        let activeColor;
+        let colors = this.controlFColor();
+        let colorActive = colors[0].getElementsByClassName('color_active')[0];
+        if (colorActive.getAttribute('data-text-color') === 'black')
+            activeColor = 'book_color-black';
+        else if (colorActive.getAttribute('data-text-color') === 'gray')
+            activeColor = 'book_color-gray';
+        else
+            activeColor = 'book_color-whitesmoke';
+        if (this.htmlBlock.classList.contains(activeColor) === false) {
+            for (let elem of ['book_color-black', 'book_color-gray', 'book_color-whitesmoke']) {
+                if (activeColor !== elem && this.htmlBlock.classList.contains(elem)) {
+                    this.htmlBlock.classList.remove(elem);
+                    break;
+                }
+            }
+            this.htmlBlock.classList.add(activeColor);
+        }
+        return;
+    }
+    /* font color - end */
+    /* font background-color - start */
+    bakgroundСolor() {
+        return this.htmlBlock.getElementsByClassName('book__control_background');
+    }
+    removeActiveBackg() {
+        /*
+        TODO: The background 'color_active' class finding and remove active classe.
+        */
+        let backgBlock = this.bakgroundСolor();
+        let activeBg = backgBlock[0].getElementsByClassName('color');
+        let i = 0;
+        for (i; i < activeBg.length; i++) {
+            if (activeBg[i].classList.contains('color_active')) {
+                activeBg[i].classList.remove('color_active');
+                return i;
+            }
+        }
+        return;
+    }
+    findBackgClick() {
+        /*
+        TODO: the element finding wich has an event. It's for assingment the 'active' class
+        */
+        let backgBlock = this.bakgroundСolor();
+        let refColor = backgBlock[0].getElementsByClassName('color');
+        let ind = 0;
+        for (ind; ind < refColor.length; ind++) {
+            let elem = refColor[ind];
+            refColor[ind].addEventListener('click', () => {
+                this.removeActiveBackg();
+                if (elem.classList.contains('color_active') === false) {
+                    elem.classList.add('color_active');
+                    this.activBackgColor();
+                }
+            });
+        }
+        return;
+    }
+    activBackgColor() {
+        /*
+        
+        TODO: the new element wich has get the new  class 'active'
+        
+        */
+        let activeBg;
+        let backgBlock = this.bakgroundСolor();
+        let backgActive = backgBlock[0].getElementsByClassName('color_active')[0];
+        if (backgActive.getAttribute('data-bg-color') === 'black')
+            activeBg = 'book_bg-black';
+        else if (backgActive.getAttribute('data-bg-color') === 'gray')
+            activeBg = 'book_bg-gray';
+        else
+            activeBg = 'book_bg-white';
+        if (this.htmlBlock.classList.contains(activeBg) === false) {
+            for (let elem of ['book_bg-black', 'book_bg-gray', 'book_bg-white']) {
+                if (activeBg !== elem && this.htmlBlock.classList.contains(elem)) {
+                    this.htmlBlock.classList.remove(elem);
+                    break;
+                }
+            }
+            this.htmlBlock.classList.add(activeBg);
+        }
+        return;
+    }
+    /* font background-color - end */
     startUp() {
         this.blockReference();
-        this.findSizeClick();
-        this.fontColor();
+        this.findFSizeClick();
+        this.findColorClick();
+        this.findBackgClick();
     }
 }
 const block_book = document.getElementById('book');
