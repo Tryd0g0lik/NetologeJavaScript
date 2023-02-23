546e1032fe738af4c1354aa742a0972f8292ff4b
Tryd0g0lik <work80@mail.ru>
2023-02-23 16:19:25 +0700

    the corrected code

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,40 +1,54 @@
-// Согласно условию Видим реальное время в чате.
-// чатБот Алиса обчается )) в разговоре с нами. 
-// Если в input поле оставляем пустое и нажимаем на Enter, получаем событие: 
-// чат закрывается и сбрасивается.
-// Как , данное событие/условие взять под контроль ?  Где это вообще прописано?
+// Время поменял, теперь час : минута. В задании не нашел уточнений , что берем за время.
 var chatBlock = document.getElementsByClassName('chat-widget');
-var inputIn = document.getElementById('chat-widget__input');
-var chatMassege = document.getElementById('chat-widget__messages');
-var speak = [
+var elementInput = document.getElementById('chat-widget__input');
+var chatMassage = document.getElementById('chat-widget__messages');
+var speskingBot = [
     "Кто тут?",
     "Где ваша совесть?",
     "Пока",
     'Привет!'
 ];
-var fielt = inputIn;
-fielt.setAttribute('min', "1");
-fielt.setAttribute("max", "100");
-chatBlock[0].addEventListener('click', function (e) {
-    var element = e.target;
-    element.closest('.chat-widget').classList.add('chat-widget_active');
-});
-var chat = '';
-inputIn.addEventListener('keydown', function (e) {
-    if (e.code === 'Enter' || e.code === 'NumpadEnter' && fielt.value.length > 0 && fielt.value.length <= 100) {
-        e.preventDefault();
-        var date = new Date();
-        chat += "\n\t\t\t<div class=\"message message_client\">\n\t\t\t\t\t<div class=\"message__time\">".concat(date.getMinutes(), ":").concat(date.getSeconds(), "</div>\n\t\t\t\t\t<div class=\"message__text\">").concat(fielt.value, "</div>\n\t\t\t</div>\n\t\t");
-        setTimeout(function () {
-            chat += alisa();
-            chatMassege.innerHTML = chat;
-        }, 500);
-        chatMassege.innerHTML = chat;
-        fielt.value = '';
+var field;
+
+if (elementInput) {
+    field = elementInput;
+    field.setAttribute('min', "1");
+    field.setAttribute("max", "100");
+}
+
+if (chatBlock) {
+    chatBlock[0].addEventListener('click', function (e) {
+        var element = e.target;
+
+        if (element.closest('.chat-widget')) {
+            element.closest('.chat-widget').classList.add('chat-widget_active');
+        }
+    });
+}
+
+var chat = ''; // FOr save histore of html-dialgue between a bot and user.
+elementInput.addEventListener('keydown', function (e) {
+    if (e.code.includes('Enter') || e.code.includes("NumpadEnter")) {
+        if (field.value.trim().length !== 0 &&
+            field.value.trim().length <= 100) {
+            e.preventDefault();
+
+            var date = new Date();
+            chat += "\n\t\t\t<div class=\"message message_client\">\n\t\t\t\t\t<div class=\"message__time\">".concat(date.getHours(), ":").concat(date.getMinutes(), "</div>\n\t\t\t\t\t<div class=\"message__text\">").concat(field.value, "</div>\n\t\t\t</div>\n\t\t");
+            setTimeout(function () {
+                chat += selectStringForChat();
+                chatMassage.innerHTML = chat;
+            }, 500);
+
+            if (chatMassage) {
+                chatMassage.innerHTML = chat;
+                field.value = '';
+            }
+        }
     }
 });
-function alisa() {
-    var i = Math.floor(Math.random() * speak.length);
+function selectStringForChat() {
+    var i = Math.floor(Math.random() * speskingBot.length);
     var date = new Date();
-    return "<div class=\"message\">\n\t<div class=\"message__time\">".concat(date.getMinutes(), ":").concat(date.getSeconds(), "</div>\n\t<div class=\"message__text\">").concat(speak[i], "</div>\n</div>");
+    return "<div class=\"message\">\n\t<div class=\"message__time\">".concat(date.getHours(), ":").concat(date.getMinutes(), "</div>\n\t<div class=\"message__text\">").concat(speskingBot[i], "</div>\n</div>");
 }
