165dac0f27b97b6633dba8f7bac2b1c685523c6e
Tryd0g0lik <work80@mail.ru>
2023-03-05 13:59:40 +0700

    task4_1: rewrited a 'gulp-progressbar/progressbar' task code and to revrited .gitignore files

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -4,8 +4,6 @@ const buttons = document.getElementById('send');
 const f = document.getElementById('form');
 const progr = document.getElementById('progress');
 // метод "FormData" используем при отправке данных ТОЛЬКО если есть строковые <input texterea полы ИЛИ при какиих условиях? 
-const file = document.getElementById('file');
-
 //Просто не понима для чего указывать 'action'.
 if (f.hasAttribute('action')) {
     f.removeAttribute('action');
@@ -15,55 +13,39 @@ class Http {
         this.url = url;
         this.posts = 'POST';
     }
-
     // Создаем модель для загрузки.
     modelAjax() {
         // Просто копируем данные
         let methods = Array.from([this.posts])[0];
         let urls = Array.from([this.url])[0];
-
         if (window.XMLHttpRequest) {
             this.request = new XMLHttpRequest();
-            this.request.overrideMimeType('text/xml');
         }
         else if (window.ActiveXObject) {
             this.request = new ActiveXObject("Microsoft.XMLHTTP");
         }
-
         if (!this.request) {
             alert('1: You are a loser!. Your "request" is not worlk');
         }
         ;
-
+        const formData = new FormData(f);
         this.request.open(methods, urls);
         this.request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        this.request.send(`file = ${file.value}`);
-
         /* ------------start----------------- */
         // Загрузка - получаем размер загруженного
         this.request.upload.onprogress = (event) => {
             this.request.onreadystatechange = () => {
-                console.log(this.request.readyState);
-                console.log("2: ", file.value);
-                console.log(event.lengthComputable);
-
-                if (this.request.readyState === 2 && event.lengthComputable) {
-                    let f = event.loaded;
-                    console.log("event.loaded: ", event.loaded);
-                    progr.value = f;
-                    console.log(`Отправлено ${event.loaded} из ${event.total}`);
-                    console.log((event.loaded / event.total) * 100);
-
+                if (this.request.readyState === 2) {
+                    let resp = event.loaded;
+                    progr.value = resp;
                     if (event.loaded === event.total) {
                         contents[0].insertAdjacentHTML('beforeend', 'Данные загружены!');
                     }
                 }
             };
         };
+        this.request.send(formData);
         /* --------------completed--------------- */
-
-        if (this.request.readyState === this.request.DONE) {
-        }
     }
     giveDownloadStart() {
         buttons.addEventListener('click', (e) => {
