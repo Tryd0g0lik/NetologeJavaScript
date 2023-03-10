b5d3559ddc0979cc94418879f9852477058a00ca
Tryd0g0lik <work80@mail.ru>
2023-03-10 16:34:33 +0700

    task4_1: rewrite a 'progressbar' code

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,23 +1,23 @@
-"use strict";
-const contents = document.getElementsByClassName('content');
-const buttons = document.getElementById('send');
-const f = document.getElementById('form');
-const progr = document.getElementById('progress');
+var contents = document.getElementsByClassName('content');
+var buttons = document.getElementById('send');
+var f = document.getElementById('form');
+var progr = document.getElementById('progress');
 // метод "FormData" используем при отправке данных ТОЛЬКО если есть строковые <input texterea полы ИЛИ при какиих условиях? 
 //Просто не понима для чего указывать 'action'.
 if (f.hasAttribute('action')) {
     f.removeAttribute('action');
 }
-class Http {
-    constructor(url) {
+var Http = /** @class */ (function () {
+    function Http(url) {
         this.url = url;
         this.posts = 'POST';
     }
     // Создаем модель для загрузки.
-    modelAjax() {
+    Http.prototype.modelAjax = function () {
+        var _this = this;
         // Просто копируем данные
-        let methods = Array.from([this.posts])[0];
-        let urls = Array.from([this.url])[0];
+        var methods = Array.from([this.posts])[0];
+        var urls = Array.from([this.url])[0];
         if (window.XMLHttpRequest) {
             this.request = new XMLHttpRequest();
         }
@@ -28,34 +28,34 @@ class Http {
             alert('1: You are a loser!. Your "request" is not worlk');
         }
         ;
-        const formData = new FormData(f);
+        var formData = new FormData(f);
         this.request.open(methods, urls);
         this.request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
         /* ------------start----------------- */
         // Загрузка - получаем размер загруженного
-        this.request.upload.onprogress = (event) => {
-            this.request.onreadystatechange = () => {
-                if (this.request.readyState === 2) {
-                    let resp = event.loaded;
-                    progr.value = resp;
+        this.request.upload.onprogress = function (event) {
+            ;
+            progr.value = (event.loaded / event.total).toFixed(2);
                     if (event.loaded === event.total) {
                         contents[0].insertAdjacentHTML('beforeend', 'Данные загружены!');
                     }
-                }
-            };
+            //     }
+            // };
         };
         this.request.send(formData);
         /* --------------completed--------------- */
-    }
-    giveDownloadStart() {
-        buttons.addEventListener('click', (e) => {
+    };
+    Http.prototype.giveDownloadStart = function () {
+        var _this = this;
+        buttons.addEventListener('click', function (e) {
             e.preventDefault();
-            this.modelAjax();
+            _this.modelAjax();
         });
-    }
-}
-const url = 'https://students.netoservices.ru/nestjs-backend/upload';
-const http = new Http(url);
+    };
+    return Http;
+}());
+var url = 'https://students.netoservices.ru/nestjs-backend/upload';
+var http = new Http(url);
 http.giveDownloadStart();
 
 //# sourceMappingURL=task.js.map
