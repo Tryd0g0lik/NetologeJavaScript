81340a4b63c46899b0615d2254aa1de7adf00b8c
Tryd0g0lik <work80@mail.ru>
2023-03-13 13:44:00 +0700

    task5_2 stay on pause

---
+++ modules_Webpack/webpack.config.js
@@ -0,0 +1,32 @@
+// В синтаксисе config.js есть '[]'. В документации
+// вижу их в разных свойства. Пример "..: '[id].js'," || " ..: '[name].css'". 
+// Какое у них предназначение? 
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      }
+    ]
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      minify: false,
+
+      filename: 'main.html',
+      template: path.resolve(__dirname, './src/html/index.html'),
+      base: "https://example.com/",
+      title: 'firstApp_Webpack',
+
+    }),
+    new MiniCssExtractPlugin(
+      { filename: "./css/main.css" }
+    )]
+}
