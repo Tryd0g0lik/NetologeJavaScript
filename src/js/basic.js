29ff48d96f84ae344029f7b8569dbf8285a183de
Denis <63048913+Tryd0g0lik@users.noreply.github.com>
2023-03-23 11:32:22 +0700

    Update basic.js
    received error from 'no-nested-ternary' rules  was been corrected

--- src/js/basic.js
+++ src/js/basic.js
@@ -14,8 +14,15 @@ export class Hero {
   }
 
   get statusHealtly() {
-    this.healthyHero = (this.health > 50) ? 'healthy'
-      : (this.health >= 15 && this.healyh <= 50) ? 'wounded' : 'critical';
-    return this.healthyHero;
+    let healthyHero;
+    if (this.health > 50) {
+      healthyHero = 'healthy';
+    } elsif (this.health >= 15 && this.healyh <= 50){
+      healthyHero = 'wounded';
+    } else {
+      healthyHero = 'critical';
+    }
+    this.healthyHero = healthyHero;
+    return healthyHero;
   }
 }
