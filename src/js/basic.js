b9e56065030601497468b7243fbf5d354258929c
Tryd0g0lik <work80@mail.ru>
2023-03-23 10:31:35 +0700

    maked the class Hero inside the basic tab

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,7 +1,26 @@
-export default function sum(items) {
+export function sum(items) {
   let result = 0;
   for (const item of items) {
     result += item;
   }
   return result;
+}
+
+export class Hero {
+  constructor(hero) {
+    this.name = hero.name;
+    this.health = hero.health;
+
+  }
+
+  // set statusHealth(number) {
+
+  // }
+  get statusHealtly() {
+    this.healthyHero = (this.health > 50) ? 'healthy' :
+      (this.health >= 15 && this.healyh <= 50) ? 'wounded' : 'critical';
+    let healthyHero = this.healthyHero;
+
+    return healthyHero
+  }
 }
