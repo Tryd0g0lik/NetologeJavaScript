d5a4cab23c6ed6a0154485cf11feab90222d535d
Denis <63048913+Tryd0g0lik@users.noreply.github.com>
2023-03-23 11:06:21 +0700

    Update basic.js
    basic.js - corrected

--- src/js/basic.js
+++ src/js/basic.js
@@ -4,7 +4,8 @@ export function sum(items) {
     result += item;
   }
   return result;
-}
+};
+
 
 export class Hero {
   constructor(hero) {
@@ -13,14 +14,10 @@ export class Hero {
 
   }
 
-  // set statusHealth(number) {
-
-  // }
   get statusHealtly() {
-    this.healthyHero = (this.health > 50) ? 'healthy' :
-      (this.health >= 15 && this.healyh <= 50) ? 'wounded' : 'critical';
-    let healthyHero = this.healthyHero;
-
-    return healthyHero
-  }
-}
+    this.healthyHero = (this.health > 50) ? 'healthy'
+    : (this.health >= 15 && this.healyh <= 50) ? 'wounded' : 'critical';
+    
+    return this.healthyHero;
+  };
+};
